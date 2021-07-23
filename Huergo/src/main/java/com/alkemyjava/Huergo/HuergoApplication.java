7208a10634407b81910150eb486d23d3481642bd
bhuergo <barbarahuergo@gmail.com>
2021-07-23 16:49:42 -0300

    Entities

---
+++ Huergo/src/main/java/com/alkemyjava/Huergo/HuergoApplication.java
@@ -0,0 +1,13 @@
+package com.alkemyjava.Huergo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuergoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HuergoApplication.class, args);
+	}
+
+}
---
+++ Huergo/src/test/java/com/alkemyjava/Huergo/HuergoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alkemyjava.Huergo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuergoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
