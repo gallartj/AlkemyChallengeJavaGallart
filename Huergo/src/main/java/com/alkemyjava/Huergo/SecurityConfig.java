9a8495af22ac66f7c12809b00a41482407997396
bhuergo <barbarahuergo@gmail.com>
2021-07-23 17:50:26 -0300

    SecurityConfig

--- Huergo/src/main/java/com/alkemyjava/Huergo/SecurityConfig.java
+++ Huergo/src/main/java/com/alkemyjava/Huergo/SecurityConfig.java
@@ -1,8 +1,13 @@
 package com.alkemyjava.Huergo;
 
+import com.alkemyjava.Huergo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -12,8 +17,43 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private UserService userService;
+
     @Bean
     public BCryptPasswordEncoder encoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Autowired
+    public void configurerGlobal(AuthenticationManagerBuilder builder) throws Exception {
+        builder.userDetailsService(userService).passwordEncoder(encoder());
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers().permitAll()
+                .antMatchers().hasRole("ADMIN").antMatchers().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/")
+                .loginProcessingUrl("/login")
+                .defaultSuccessUrl("index").permitAll()
+                .and()
+                .logout()
+                .logoutUrl("/logout")
+                .invalidateHttpSession(true)
+                .deleteCookies("JSESSIONID")
+                .logoutSuccessUrl("/")
+                .and()
+                .csrf().disable();
     }
 }
---
+++ Huergo/src/main/java/com/alkemyjava/Huergo/enumerations/Role.java
@@ -0,0 +1,5 @@
+package com.alkemyjava.Huergo.enumerations;
+
+public enum Role {
+    ADMIN, USER
+}
